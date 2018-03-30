f70513e749d505afc16e665c358865f2352aa7b8
ZITOUNI <zitouni@zitounis-mbp.boiscolombes.fr.ibm.com>
2018-03-30 12:11:08 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
 import { Component } from '@angular/core';
+import { TodoDataService } from './todo-data.service';
+import { Todo } from './todo';
+import { trigger, transition, animate, style } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('ease-in-out', [
+      transition(':enter', [
+        style({transform: 'translateY(-20px)', opacity: 0}),
+        animate(250)
+      ]),
+
+      transition(':leave', [
+        animate(250, style({transform: 'translateY(-20px)', opacity: 0}))
+      ])
+    ]),
+
+    trigger('move-up', [
+      transition('* => *', [
+        animate(250)
+      ])
+    ])
+  ]
+
 })
 export class AppComponent {
-  title = 'app';
+
+  newTodo: Todo = new Todo();
+
+  constructor(private todoDataService: TodoDataService) {
+
+  }
+  
+  addTodo() {
+    if (this.newTodo.title !== '') {
+      this.todoDataService.addTodo(this.newTodo);
+      this.newTodo = new Todo();
+    }
+  }
+  
+  toggleTodoComplete(todo) {
+    this.todoDataService.toggleTodoComplete(todo);
+  }
+  
+  removeTodo(todo) {
+    this.todoDataService.deleteTodoById(todo.id);
+  }
+
+  get todos() {
+    return this.todoDataService.getAllTodos();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+
+import { TodoDataService } from './todo-data.service';
 
 
 @NgModule({
@@ -10,9 +13,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [TodoDataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
